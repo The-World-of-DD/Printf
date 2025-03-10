9703f6e924f671c2d25fda70ee9ec0740c85fc28
DIegoR <128084117+The-World-of-DD@users.noreply.github.com>
2025-03-10 22:07:59 +0100

    hhhghgh

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dierojas <dierojas@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 19:53:22 by dierojas          #+#    #+#             */
-/*   Updated: 2025/03/08 20:45:37 by dierojas         ###   ########.fr       */
+/*   Updated: 2025/03/10 22:07:44 by dierojas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,18 @@ int	ft_printf(char const *, ...)
 	
 }
 
+#include <stdio.h>
 int	main()
 {
-	
+    printf("PROBANDO CARÁCTER -> %c\n", 'c');
+/*     printf("PROBANDO STRING -> %s\n", "sherlocked");
+    printf("PROBANDO VOID -> %p\n", (void *)"holo");
+    printf("PROBANDO DECIMAL -> %d\n", 123345);
+    printf("PROBANDO ENTERO -> %i\n  %i\n", 5678, -5678);
+    printf("PROBANDO NUMERO DECIMAL UNSIGNED %u\n", -2345);
+    printf("PROBANDO HEX MIN %x\n", 2345);
+    printf("PROBANDO HEX MAY %X\n", 2345);
+    printf("PROBANDO PORCENTAJE %%\n"); */
+
+    return 0;
 }
