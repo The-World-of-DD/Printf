5c7e3514ad9e1f6bc85c14d345088b27dc7dc6e4
DiegoR <128084117+The-World-of-DD@users.noreply.github.com>
2025-03-27 12:09:24 +0000

    hhh

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: dierojas < dierojas@student.42madrid.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 19:53:22 by dierojas          #+#    #+#             */
-/*   Updated: 2025/03/27 11:35:23 by dierojas         ###   ########.fr       */
+/*   Updated: 2025/03/27 12:09:22 by dierojas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,7 @@ int	ft_printf(char const *s, ...)
 	va_end(args);
 	return (0);
 }
-
+/* 
 #include <stdio.h>
 
 int main() 
@@ -90,3 +90,4 @@ int main()
 
     return 0;
 } 
+ */
